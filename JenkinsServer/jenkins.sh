2fba12a135cb8b15d528e7c22addb6fc3475418a
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-12-06 09:14:10 +0530

    Create jenkins.sh

---
+++ JenkinsServer/jenkins.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+########### Install Jenkins ############
+sudo apt update
+
+sudo apt install openjdk-11-jre-headless -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt install awscli -y
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo systemctl start jenkins 
+sudo systemctl enable jenkins
+
+
+
+echo "Kind installed successfully !!!!!!!!!!!!!!!"
+########## Install Kubectl ############
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+# echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update -q && \z
+sudo apt-get install kubectl=1.20.*
+
